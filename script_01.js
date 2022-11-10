4a47bb5416b945431ec8f2b4743f2194c7261d9c
Hindax <haar162@more2learn.de>
2022-11-10 09:23:10 +0100

    if statement

--- script_01.js
+++ script_01.js
@@ -7,16 +7,26 @@ const ageJohn = 35;
 const ageMark = 30;
 
 // Deklaration
-let isJohnOlder, isJohnEqual;
+let isJohnOlder, isJohnEqual; 
 
 // Test | Logische Aussage
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
+
+/******************IF********* */
+// TINA --> There is no alternative!
+// entweder JA oder nix.. alternativlos
+
+if(isJohnOlder) // insert condition here()
+{
+    console.log("John ist älter.");
+}
+
 
